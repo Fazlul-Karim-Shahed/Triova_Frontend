c43551d69fb73d7b34e874a6ec2b4112a8b2633c
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2025-07-19 16:10:03 +0600

    check

--- src/components/Client/Facebook/Messenger.js
+++ src/components/Client/Facebook/Messenger.js
@@ -1,13 +1,38 @@
-
 "use client";
 
-import React from "react";
-import { FacebookProvider, CustomChat } from "react-facebook";
+import { useEffect } from "react";
 
-export default function Messenger() {
+export default function MessengerChat() {
+    useEffect(() => {
+        // Load the SDK only once
+        if (document.getElementById("facebook-jssdk")) return;
+
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                xfbml: true,
+                version: "v17.0", // or latest available version
+            });
+        };
+
+        const script = document.createElement("script");
+        script.id = "facebook-jssdk";
+        script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        document.body.appendChild(script);
+    }, []);
+
     return (
-        <FacebookProvider appId="361765999937307" chatSupport>
-            <CustomChat pageId="747421228445481" minimized={false} />
-        </FacebookProvider>
+        <>
+            <div id="fb-root"></div>
+
+            {/* This div must be placed where you want the Messenger chat to appear */}
+            <div
+                className="fb-customerchat"
+                attribution="biz_inbox"
+                page_id="747421228445481"
+                theme_color="#0084FF"
+                logged_in_greeting="Hi! How can we help you?"
+                logged_out_greeting="Hi! Please log in to chat with us."
+            ></div>
+        </>
     );
 }
